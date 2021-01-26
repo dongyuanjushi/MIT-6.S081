ea3a9465c10f770bb8e0db584ce0b2db49b02bdd
梅凯 <181840164@smail.nju.edu.cn>
2021-01-26 17:35:29 +0800

    pingpong finished!

--- user/pingpong.c
+++ user/pingpong.c
@@ -7,14 +7,18 @@ int main(int argc,char *argv[]){
 	pipe(p);
 	if(fork()!=0){
 		write(p[1],"ping\n",5);
-		wait(0);
+		close(p[1]);
+		wait(0); //block parent and ensure children run first!
 		read(p[0],buffer,5);
 		printf("%d: received %s",getpid(),buffer);
+		close(p[0]);
 	}
 	else{
 		read(p[0],buffer,5);
+		close(p[0]);
 		printf("%d: received %s",getpid(),buffer);
 		write(p[1],"pong\n",5);
+		close(p[1]);
 	}
 	exit(0);
 }
